620149770cbe6c02f65c8c1961032c34f61bf58e
wlcountrychick11 <60484092+wlcountrychick11@users.noreply.github.com>
2020-01-30 14:42:16 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="Refresh" content="5;url=http://www.surfingmuseum.org">
+<meta http-equiv="Refresh" content="5;url=https://www.chase.com/news/103117-chargebacks2">
 </head>
 
